82225d7ed305e22172b45bae2c140477dfc5c9a7
Sree Chandana Madamanchi <75236077+sreechandanam@users.noreply.github.com>
2023-08-01 16:14:02 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,240 @@
+﻿<!DOCTYPE html>
+<?php
+include 'constants.php';
+?>
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=devidev-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php echo @$title; ?></title>
+
+
+    <!-- [ FONT-AWESOME ICON ] 
+        
+=========================================================================================================================-->
+
+    <link rel="stylesheet" type="text/css" href="library/font-awesome-4.3.0/css/font-awesome.min.css">
+
+
+    <!-- [ PLUGIN STYLESHEET ]
+        
+=========================================================================================================================-->
+
+    <link rel="shortcut icon" type="image/x-icon" href="images/icon.png">
+
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+
+    <link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+
+    <link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+
+    <!-- [ Boot STYLESHEET ]
+        
+=========================================================================================================================-->
+
+    <link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
+
+    <link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
+
+    <!-- [ DEFAULT STYLESHEET ] 
+        
+=========================================================================================================================-->
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+    <link rel="stylesheet" type="text/css" href="css/color/themecolor.css">
+
+
+</head>
+
+<body>
+
+    <!-- [ LOADERs ]
+
+================================================================================================================================-->
+
+    <div class="preloader">
+
+        <div class="loader theme_background_color">
+
+            <span></span>
+
+
+        </div>
+    </div>
+    <!-- [ /PRELOADER ]
+
+=============================================================================================================================-->
+
+    <!-- [WRAPPER ]
+
+=============================================================================================================================-->
+
+    <div class="wrapper">
+
+        <!-- [NAV]
+ 
+============================================================================================================================-->
+
+        <!-- Navigation
+    ==========================================-->
+
+        <nav class=" nim-menu navbar navbar-default navbar-fixed-top">
+
+            <div class="container">
+
+                <!-- Brand and toggle get grouped for better mobile display -->
+
+                <div class="navbar-header">
+
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1">
+
+                        <span class="sr-only">Toggle navigation</span>
+
+                        <span class="icon-bar"></span>
+
+                        <span class="icon-bar"></span>
+
+                        <span class="icon-bar"></span>
+
+                    </button>
+
+                    <a class="navbar-brand" href="index.php"><?php echo $title[0]; ?><span class="themecolor">
+                            <?php echo $title[1]; ?></span><?php for ($i = 2; $i < strlen($title); $i++) echo $title[$i]; ?></a>
+
+                </div>
+
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                    <ul class="nav navbar-nav navbar-right">
+
+                        <li><a href="#home" class="page-scroll">
+                                <h3>Home</h3>
+                            </a></li>
+
+                        <li><a href="#two" class="page-scroll">
+                                <h3>About</h3>
+                            </a></li>
+
+                        <li><a href="library/sample.pdf" class="page-scroll">
+                                <h3>Map</h3>
+                            </a></li>
+
+                        <li><a href="pro/signin.php" class="page-scroll">
+                                <h3>Passenger Portal</h3>
+                            </a></li>
+
+                        <li><a href="pro/adminsignin.php" class="page-scroll">
+                                <h3>Admin</h3>
+                            </a></li>
+                    </ul>
+
+                </div>
+                <!-- /.navbar-collapse -->
+
+            </div><!-- /.container-fluid -->
+
+        </nav>
+
+
+
+        <!-- [/NAV]
+ 
+============================================================================================================================-->
+
+
+        <!-- [/MAIN-HEADING]
+ 
+============================================================================================================================-->
+
+        <section class="main-heading" id="home">
+
+            <div class="overlay">
+
+                <div class="container">
+
+                    <div class="row">
+
+                        <div class="main-heading-content col-md-12 col-sm-12 text-center">
+
+                            <h1 class="main-heading-title"><span class="main-element themecolor"
+                                    data-elements=" Online Ticket, Online Ticket, Online Ticket"></span></h1>
+
+                            <h1 class="main-heading-title"><span class="main-element themecolor"
+                                    data-elements=" Reservation System, Reservation System, Reservation System"></span>
+                            </h1>
+
+                            <p class="main-heading-text">WELCOME TO,<br />E - TICKETING FOR RAILWAYS</p>
+
+                            <div class="btn-bar">
+
+                                <a href="pro/signin.php" class="btn btn-custom theme_background_color">Make Reservations Now</a>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </section>
+
+
+    <!-- [ DEFAULT SCRIPT ] -->
+
+    <script src="library/modernizr.custom.97074.js"></script>
+
+    <script src="library/jquery-1.11.3.min.js"></script>
+
+    <script src="library/bootstrap/js/bootstrap.js"></script>
+
+    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+
+    <!-- [ PLUGIN SCRIPT ] -->
+
+    <script src="library/vegas/vegas.min.js"></script>
+
+    <script src="js/plugins.js"></script>
+
+    <!-- [ TYPING SCRIPT ] -->
+
+    <script src="js/typed.js"></script>
+
+    <!-- [ COUNT SCRIPT ] -->
+
+    <script src="js/fappear.js"></script>
+
+    <script src="js/jquery.countTo.js"></script>
+
+    <!-- [ SLIDER SCRIPT ] -->
+
+    <script src="js/owl.carousel.js"></script>
+
+    <script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+
+    <script type="text/javascript" src="js/SmoothScroll.js"></script>
+
+
+    <!-- [ COMMON SCRIPT ] -->
+    <script src="js/common.js"></script>
+
+</body>
+
+
+</html>
